b87aceb1f5792d15bd780e75a3a0ed1202d60981
Pratim Ugale <pratim.ugale@gmail.com>
2018-06-22 20:19:26 +0530

    Converting a section of Image

---
+++ operations/basic_image_operations.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('watch.jpg', cv2.IMREAD_COLOR)
+
+px = img [55,55]
+
+img[100:150, 100:150] = [0,0,0]
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print (px)
